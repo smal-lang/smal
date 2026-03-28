e82b08280e0ec991b20e6704584fbd8de9673741
Jonathan Bailey <jonathan.bailey@comcast.net>
2026-03-28 02:08:24 -0400

    Got basic SM diagramming working

--- smal/diagramming/generation.py
+++ smal/diagramming/generation.py
@@ -1,14 +1,25 @@
 from graphviz import Digraph
 from smal.schemas.smal_file import SMALFile
+from pathlib import Path
 
-def generate_state_machine_svg(smal_path: str, output_svg_path: str) -> None:
+def generate_state_machine_svg(smal_path: str | Path, svg_output_dir: str | Path) -> None:
+    smal_path = Path(smal_path)
+    if not smal_path.is_file():
+        raise ValueError(f"Invalid filepath for SMAL file: {smal_path}")
+    svg_output_dir = Path(svg_output_dir)
+    if not svg_output_dir.is_dir():
+        raise ValueError(f"Invalid SVG output dir: {svg_output_dir}")
+
+    # Parse the SMAL file
     smal = SMALFile.from_file(smal_path)
+
+    # Create a graphviz Digraph using the SMAL file
     dot = Digraph(
         name=smal.machine,
         format="svg",
-        graph_attr={"rankdir": "LR", "fontsize": "10", "fontname": "Arial"},
-        node_attr={"shape": "ellipse", "style": "filled", "fillcolor": "#f0f0f0"},
-        edge_attr={"fontsize": "9"},
+        graph_attr={"rankdir": "LR", "fontsize": "12", "fontname": "Arial"},
+        node_attr={"shape": "box", "style": "filled", "fillcolor": "#f0f0f0"},
+        edge_attr={"fontsize": "10"},
     )
 
     # Add states
@@ -22,4 +33,5 @@ def generate_state_machine_svg(smal_path: str, output_svg_path: str) -> None:
             label += f" → {t.landing_state_entry_evt}"
         dot.edge(t.trigger_state, t.landing_state, label=label)
 
-    dot.render("machine.svg", cleanup=True)
+    # Render the SVG file
+    dot.render(filename=smal.machine.lower(), directory=svg_output_dir, cleanup=True)
